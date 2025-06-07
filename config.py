c3f8392160b9185a63ffc27ddaf6fdbee56c1464
BADNAM XD <yadavxd746@gmail.com>
2025-06-07 12:20:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "27079591")) #optional
+API_ID = int(getenv("API_ID", "7757427805")) #optional
 API_HASH = getenv("API_HASH", "c81ae4c3dc026ea4bf49842a8ce4a5f9") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8130531095").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7757427805").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
